cd071021ac484f1bdc92ca11256b3fd8905b3162
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-10-05 17:49:48 +0900

    Fix filter()

--- denops/@ddc-sources/cmdline-history.ts
+++ denops/@ddc-sources/cmdline-history.ts
@@ -19,7 +19,8 @@ export class Source extends BaseSource<{}> {
       "ddc_cmdline_history#get",
     ) as string[];
     const inputLength = args.context.input.length - args.completeStr.length;
-    return histories.filter((word) => word.length >= inputLength)
+    const input = args.context.input.substring(0, inputLength);
+    return histories.filter((word) => word.startsWith(input))
       .map((word) => ({ word: word.substring(inputLength) }));
   }
 
