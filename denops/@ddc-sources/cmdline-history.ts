58f6095f55a7b467077aa59cd15dc08ada5592f9
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-10-12 13:43:19 +0900

    Add maxSize params

--- denops/@ddc-sources/cmdline-history.ts
+++ denops/@ddc-sources/cmdline-history.ts
@@ -7,24 +7,33 @@ import {
 } from "https://deno.land/x/ddc_vim@v0.15.0/types.ts#^";
 import { Denops } from "https://deno.land/x/ddc_vim@v0.15.0/deps.ts#^";
 
-export class Source extends BaseSource<{}> {
+type Params = {
+  maxSize: number;
+};
+
+export class Source extends BaseSource<Params> {
   async gatherCandidates(args: {
     denops: Denops;
     context: Context;
     options: DdcOptions;
     sourceOptions: SourceOptions;
+    sourceParams: Params,
     completeStr: string;
   }): Promise<Candidate[]> {
+    const p = args.sourceParams as unknown as Params;
+    const maxSize = p.maxSize;
     const histories = await args.denops.call(
       "ddc_cmdline_history#get",
     ) as string[];
     const inputLength = args.context.input.length - args.completeStr.length;
     const input = args.context.input.substring(0, inputLength);
-    return histories.filter((word) => word.startsWith(input))
+    return histories.slice(0, maxSize).filter((word) => word.startsWith(input))
       .map((word) => ({ word: word.substring(inputLength) }));
   }
 
-  params(): {} {
-    return {};
+  params(): Params {
+    return {
+      maxSize: 1000,
+    };
   }
 }
